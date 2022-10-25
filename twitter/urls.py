31dc19da732f44e620d40b99a5d53739947ac2b0
maximeKets <maximekets80@gmail.com>
2022-10-25 09:12:29 +0200

    feat : template

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/welcome', views.welcome, name='welcome'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
